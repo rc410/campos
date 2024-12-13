dd462aaf2c8dfd8ac44bdee328a30d1244a615a0
fiaro <fiaro@hotmail.co.jp>
2024-12-13 12:19:40 +0900

    プロジェクト ファイルを追加します。

---
+++ campos/Form1.cs
@@ -0,0 +1,74 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace Campos
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private Timer timer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count > 0)
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(Video_NewFrame);
+                videoSource.Start();
+            }
+            timer = new Timer { Interval = 1000 };
+            // 1秒ごとにバーコードをチェック
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Start();
+        }
+
+
+
+        private void Video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                BarcodeReader reader = new BarcodeReader();
+                reader.Options=  new ZXing.Common.DecodingOptions
+                {
+                    TryHarder = true,
+                    PossibleFormats = new[] { BarcodeFormat.EAN_13 }.ToList()
+                };
+                var result = reader.Decode((Bitmap)pictureBox1.Image);
+                if (result != null) {
+                    MessageBox.Show("バーコードを検出しました: " + result.Text);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            {
+                if (videoSource != null && videoSource.IsRunning)
+                {
+                    videoSource.SignalToStop(); videoSource.WaitForStop();
+                }
+            }
+        }
+    }
+}
